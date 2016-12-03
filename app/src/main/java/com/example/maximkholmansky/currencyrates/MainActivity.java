d29c6356b3156e31433f270b02015cde4cb505a0
maximkholmansky <mvh1@mail.ru>
2016-12-03 21:07:45 +0600

    add notifications

--- app/src/main/java/com/example/maximkholmansky/currencyrates/MainActivity.java
+++ app/src/main/java/com/example/maximkholmansky/currencyrates/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.maximkholmansky.currencyrates;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -58,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 executeResponse();
+                showNotification();
             }
         });
     }
@@ -100,7 +107,24 @@ public class MainActivity extends AppCompatActivity {
         ed.putFloat("myUSD",myUsd);
         ed.putFloat("myEUR",myEur);
         ed.commit();
-        Toast.makeText(this,String.valueOf(myEur)+" "+String.valueOf(myUsd), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Saved", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void showNotification() {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        Resources r = getResources();
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker("Hello")
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle("Attention!")
+                .setContentText("Курс растет")
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
     }
 
     public interface ApiService {
